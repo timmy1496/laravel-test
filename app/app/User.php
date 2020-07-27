94e31b5ed62599afbcc7df5ba6e19b7ad6f850f4
Artem Chernenko <artem.chernenko1496@gmail.com>
2020-07-27 22:11:58 +0300

    make route and controller for users

--- app/app/User.php
+++ app/app/User.php
@@ -41,4 +41,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
 }
